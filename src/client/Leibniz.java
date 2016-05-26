3896dce2ca72916d96d476d3be2b1812e847e5f8
Davi Koscianski Vidal <davividal@gmail.com>
2016-05-26 16:54:31 -0300

    Reformatando código

--- src/client/Leibniz.java
+++ src/client/Leibniz.java
@@ -3,37 +3,10 @@ package client;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
-<<<<<<< cd75430ee50d709b7a96b7b826c03f67c377c792
-public class Leibniz extends UnicastRemoteObject implements InterfaceLeibniz{
-		
-	protected Leibniz() throws RemoteException {
-		super();
-		System.out.println("Marcelo curte pirocas!");
-	}
-
-	@Override
-	public double calc(double n) throws RemoteException {
-		// TODO Auto-generated method stub
-		double pi = 0;
-		
-		for (int i = 0; i < n; i++) 
-		{
-			pi += Math.pow(-1,i)/(2*i+1);
-		}
-		return pi * 4;
-	}
-=======
 class Leibniz extends UnicastRemoteObject implements InterfaceLeibniz {
-    //private static long tempoInicio = System.currentTimeMillis();
-    //private static int number;
-
-    //double pi = computePi(number);
-    //System.out.println(pi);
-    //System.out.println("Tempo Total: " + (System.currentTimeMillis() - tempoInicio));
-
-
-    public Leibniz() throws RemoteException {
+    protected Leibniz() throws RemoteException {
         super();
+        System.out.println("Marcelo curte pirocas!");
     }
 
     @Override
@@ -46,5 +19,4 @@ class Leibniz extends UnicastRemoteObject implements InterfaceLeibniz {
         }
         return pi * 4;
     }
->>>>>>> Reformatando código
 }
