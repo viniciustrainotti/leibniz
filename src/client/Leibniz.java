2e8550f3e37a1c0f26b93d249ce77d09326f0faa
Davi Koscianski Vidal <davividal@gmail.com>
2016-05-26 17:47:18 -0300

    Melhorando tratamento de exceções

--- src/client/Leibniz.java
+++ src/client/Leibniz.java
@@ -3,23 +3,18 @@ package client;
 import java.rmi.RemoteException;
 import java.rmi.server.UnicastRemoteObject;
 
-public class Leibniz extends UnicastRemoteObject implements InterfaceLeibniz{
-		
-	protected Leibniz() throws RemoteException {
-		super();
-		System.out.println("Marcelo curte pirocas!");
-	}
+public class Leibniz extends UnicastRemoteObject implements InterfaceLeibniz {
+    protected Leibniz() throws RemoteException {
+        super();
+    }
 
-	@Override
-	public double calc(double n) throws RemoteException {
-		
-		double pi = 0;
-		
-		for (int i = 0; i < n; i++) 
-		{
-			pi += Math.pow(-1,i)/(2*i+1);
-		}
-		return pi * 4;
-	}
-	// Teste
+    @Override
+    public double calc(double n) throws RemoteException {
+        double pi = 0;
+
+        for (int i = 0; i < n; i++) {
+            pi += Math.pow(-1, i) / (2 * i + 1);
+        }
+        return pi * 4;
+    }
 }
