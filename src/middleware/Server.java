e29bef21aa8f7a95ef6d5585c75c9ea5cf8da12c
Davi Koscianski Vidal <davividal@gmail.com>
2016-05-30 00:08:17 -0300

    Adicionando middleware

---
+++ src/middleware/Server.java
@@ -0,0 +1,36 @@
+package middleware;
+
+import client.*;
+import server.*;
+
+import java.io.IOException;
+import java.net.MalformedURLException;
+import java.rmi.Naming;
+import java.rmi.RemoteException;
+import java.rmi.registry.LocateRegistry;
+
+public class Server extends Thread {
+    private static Integer porta = 2020;
+
+    public static void main(String args[]) throws IOException {
+        LocateRegistry.createRegistry(porta);
+
+        new server.Server().start();
+    }
+
+    @Override
+    public void run() {
+        super.run();
+
+        middleware.Leibniz leibniz;
+
+        try {
+            leibniz = new middleware.Leibniz();
+            Naming.rebind("//localhost:" + porta.toString() + "/Leibniz", leibniz);
+        } catch (RemoteException re) {
+            System.out.println("Ocorreu um erro ao iniciar a classe de cálculo!");
+        } catch (MalformedURLException e) {
+            System.out.println("Verifique a URL de binding.");
+        }
+    }
+}
